5fe229c950363d0897c9bda71ee2e677bebebe77
Daniel Keen <keendaniel01@gmail.com>
2018-04-21 10:50:13 +0100

    :recycle: refactor: test environment configured. test database used when rspec is run

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,7 +3,11 @@ require 'pg'
 class Bookmark
 
   def self.all
-    con = PG.connect :dbname => 'bookmark_manager'
+    if ENV['RACK_ENV'] = 'test'
+      con = PG.connect :dbname => 'bookmark_manager_test'
+    else 
+      con = PG.connect :dbname => 'bookmark_manager'
+    end
     result = con.exec "SELECT * FROM bookmarks"
     result.map do |row|
       row['url']
